e008bd1173ec564292f4ae180befad2a20d6d111
ryu.dev <comp.sci.lau@programmer.net>
2026-02-13 20:16:40 +0800

    day one of using git commit and git push, also Day 1 out of 100days

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("100 days life manager CLI");
+        System.out.println("Date: February 13, 2026");
+        System.out.println("Day 1: \"Experience is the best teacher\"");
+
+    }
+}
