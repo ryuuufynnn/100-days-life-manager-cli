bfeefad966d9ced96ecd4b92dcbcdc47863244b5
ryu.dev <laurence060723@gmail.com>
2026-02-14 19:58:10 +0800

    day 2: declaration of var and printing.

---
+++ dayTwoVar.java
@@ -0,0 +1,10 @@
+public class dayTwoVar {
+    public static void main(String[] args) {
+        String lifeManagementCLI = "LifeManagementCLI";
+        double appVersion = 1.0;
+        // print using variable8
+        System.out.println("Application: " + lifeManagementCLI);
+        System.out.println("Version: " + appVersion);
+        System.out.println("Welcome to" + lifeManagementCLI + "verison: " + appVersion);
+    }
+}
